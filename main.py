da3bc326d076c54e490fc63a4c9f079e6bb5a89e
Matvey Rotte <matveyrotte@gmail.com>
2023-07-23 05:35:39 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+import os
+from api_info import *
+
+if not os.path.exists("pyrogram_session"):
+    os.makedirs("pyrogram_session")
+
+
+app = Client(
+    "my_session",
+    workdir="pyrogram_session/",
+    api_hash=api_hash,
+    api_id=api_id,
+)
+
+CHAT_ID = "Your chat id"
+
+
+@app.on_message(filters.chat(CHAT_ID))
+def handle_group_messages(client, message):
+    if not message.from_user.is_self:
+        app.send_reaction(message.chat.id, message.id, "🗿")
+
+
+app.run()
